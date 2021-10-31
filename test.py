940722d649dac3778ec85bec6d3a327ea4fd132c
Maged Eltorkoman <newmaged@gmail.com>
2021-10-31 18:57:14 +0200

    Revert "add timeout to requirments"
    This reverts commit 7df1c25893b22e9c1693b4dd8583e4c73ac4f6fe.

--- test.py
+++ test.py
@@ -52,7 +52,6 @@ def two_sum(numbers, target):
 with open('config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-
 # Loading test_cases for different functions
 func_1_test_cases = {k: tuple(v) for k, v in
                      config['func_1']['test_cases'].items()}
